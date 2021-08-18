1c8240b727df40b30c115dddbec3ab76726c372c
David Blackwelder <github@davidblackwelder.com>
2021-08-18 15:48:04 -0400

    Add javascript to add functionality of the app

--- js/site.js
+++ js/site.js
@@ -1,1 +1,32 @@
+// Get the string from the page
+// controller function
+function getUserString() {
+	document.getElementById("alert").classList.add("invisible");
+	let userString = document.getElementById("userString").value;
+	let revString = reverseString(userString);
+	displayReversedString(revString);
+}
 
+// Reverse the string
+// logic function
+function reverseString(userString) {
+	let revString = [];
+
+	// reverse a string using a for loop
+	for (let index = userString.length - 1; index >= 0; index--) {
+		revString += userString[index];
+	}
+
+	return revString;
+}
+
+// Display the reversed string to the user
+// view function
+function displayReversedString(revString) {
+	// write to the page
+	document.getElementById(
+		"msg"
+	).innerHTML = `Your string reversed is: ${revString}`;
+	// show the alert box
+	document.getElementById("alert").classList.remove("invisible");
+}
